08d397fe4730f066497efe6b2d3b4ad29c2a9dbb
s21002 <s21002@std.it-college.ac.jp>
2022-08-24 14:04:14 +0900

    test

---
+++ app/src/main/java/jp/ac/it_college/std/s21002/webapi/MainActivity.kt
@@ -0,0 +1,12 @@
+package jp.ac.it_college.std.s21002.webapi
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
+//ppp
